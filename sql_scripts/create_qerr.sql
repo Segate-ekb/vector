17676ac6db5afaf4e4a3d4c891b9f1b3c70013fd
Ivanov Egor <Segate@inbox.ru>
2022-12-11 14:58:19 +0300

    Добавил обработку события QERR

---
+++ sql_scripts/create_qerr.sql
@@ -0,0 +1,18 @@
+CREATE TABLE <databaseName>.excpcntx(`DateTime` DateTime64(6,'Europe/Moscow'),
+                                  `duration` Int32,
+                                  `Level` Int32,
+                                  `Process` String,
+                                  `ProcessID` Int32,
+                                  `ProcessName` String,
+                                  `OSThread` Int32,
+                                  `ClientID` Int32,
+                                  `ApplicationName` String,
+                                  `ComputerName` String,
+                                  `ConnectID` Int32,
+                                  `SessionID` Int32,
+                                  `User` String,
+                                  `Query` String,
+                                  `Description` String,
+                                  `message` String,
+                                  `file` String) ENGINE = MergeTree() PARTITION BY toYYYYMM(DateTime)
+ORDER BY (DateTime);
